95ec045507a663c8c3e79f2272d56ae14188b59c
Ziyang Zhou <bbq379@hotmail.com>
2019-09-11 18:39:57 +0200

    20190910

--- src/training/crawling.py
+++ src/training/crawling.py
@@ -4,11 +4,12 @@ import lxml
 import requests
 import re
 
-# movies = 2
-number_of_review_per_rating = 8000
+movies = 2
+# number_of_review_per_rating = 9000
 files_ID = []
 files_Review = []
 review_count = []
+review_sum = 0
 train_set = []
 dev_set = []
 test_set = []
@@ -20,11 +21,13 @@ for i in range(10):
 with open("movie_metadata.csv", "r", encoding="utf-8") as f:
     f_csv = csv.DictReader(f)
     count = 0
-    for url in [row['movie_imdb_link'] for row in f_csv]:
+    rows = [row['movie_imdb_link'] for row in f_csv] if movies == -1 \
+        else [row['movie_imdb_link'] for row in f_csv][:movies]
+    for url in rows:
         movie_id = re.search('\\w*(?=\\/\\?)', url)[0]
         count += 1
         print("Movie " + str(count) + ": " + movie_id)
-        review_url = url.replace("?ref_=fn_tt_tt_1", "reviews?ref_=tt_urv")
+        review_url = url.replace("?ref_=fn_tt_tt_1", "reviews?sort=submissionDate&dir=desc&ratingFilter=0")
         response = requests.get(review_url)
         page_soup = BeautifulSoup(response.text, 'lxml')
         review_list = page_soup.find_all("div", class_="lister-item-content")
@@ -33,8 +36,8 @@ with open("movie_metadata.csv", "r", encoding="utf-8") as f:
                 rating = review_box.select('div:nth-of-type(1) > span:nth-of-type(1) > span:nth-of-type(1)')[0].text
                 rating_score = int(rating)
                 index = rating_score - 1
-                if review_count[index] >= number_of_review_per_rating:
-                    continue
+                # if review_count[index] >= number_of_review_per_rating:
+                #     continue
                 review = review_box.find("div", class_=["text", "show-more__control"]).text
             except Exception:
                 pass
@@ -42,7 +45,9 @@ with open("movie_metadata.csv", "r", encoding="utf-8") as f:
                 files_ID[index].write(movie_id + "\n")
                 files_Review[index].write("__label__" + rating + " " + review + "\n")
                 review_count[index] += 1
+                review_sum += 1
         print(review_count)
+        print("Sum: " + str(review_sum))
 
 for i in range(10):
     files_ID[i].close()
--- src/training/crawling3.py
+++ src/training/crawling3.py
@@ -4,8 +4,10 @@ import lxml
 import requests
 import re
 
-movies = 2
-number_of_reviews = 100000
+movies = 1
+number_per_rating = 10000
+number_per_movie_rating = 4
+
 files_ID = []
 files_Review = []
 review_count = []
@@ -13,9 +15,10 @@ review_sum = 0
 train_set = []
 dev_set = []
 test_set = []
+
 for i in range(10):
-    files_ID.append(open("IMDB_Crawled/data2/imdb_id_" + str(i + 1) + ".txt", "w"))
-    files_Review.append(open("IMDB_Crawled/data2/imdb_review" + str(i + 1) + ".txt", "w"))
+    files_ID.append(open("IMDB_Crawled/data3/imdb_id_" + str(i + 1) + ".txt", "w"))
+    files_Review.append(open("IMDB_Crawled/data3/imdb_review_" + str(i + 1) + ".txt", "w"))
     review_count.append(0)
 
 with open("movie_metadata.csv", "r", encoding="utf-8") as f:
@@ -28,22 +31,24 @@ with open("movie_metadata.csv", "r", encoding="utf-8") as f:
         count += 1
         print("Movie " + str(count) + ": " + movie_id)
         for i in range(10):
-            rating = i + 1
-            review_url = url.replace("?ref_=fn_tt_tt_1", "reviews?sort=helpfulnessScore&dir=desc&ratingFilter=" +
-                                     str(rating))
-            response = requests.get(review_url)
-            page_soup = BeautifulSoup(response.text, 'lxml')
-            review_list = page_soup.find_all("div", class_="lister-item-content")
-            for review_box in review_list:
-                review = review_box.find("div", class_=["text", "show-more__control"]).text
-                files_ID[i].write(movie_id + "\n")
-                files_Review[i].write("__label__" + str(rating) + " " + review + "\n")
-                review_count[i] += 1
-                review_sum += 1
-                if review_sum >= number_of_reviews:
-                    break
-            print(review_count)
-            print("Sum: " + str(review_sum))
+            if review_count[i] < number_per_rating:
+                rating = i + 1
+                review_url = url.replace("?ref_=fn_tt_tt_1", "reviews?sort=helpfulnessScore&dir=desc&ratingFilter=" +
+                                         str(rating))
+                response = requests.get(review_url)
+                page_soup = BeautifulSoup(response.text, 'lxml')
+                review_list = page_soup.find_all("div", class_="lister-item-content")[:number_per_movie_rating]
+                for review_box in review_list:
+                    review = review_box.find("div", class_=["text", "show-more__control"])
+                    review_text = re.search(r'(?<=control">)[\S\s]*(?=<\/div>)', str(review))[0]
+                    review_string = review_text.replace("<br/><br/>", "<br/>").replace("<br/>", " <NEWLINE> ")
+                    # print(review_string)
+                    files_ID[i].write(movie_id + "\n")
+                    files_Review[i].write("__label__" + str(rating) + " " + review_string + "\n")
+                    review_count[i] += 1
+                    review_sum += 1
+        print(review_count)
+        print("Sum: " + str(review_sum))
 
 for i in range(10):
     files_ID[i].close()
